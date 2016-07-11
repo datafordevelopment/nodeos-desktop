6834379d4b5388b3a71a2643c785bc189bd6f887
SpaceboyRoss01 <spaceboyross@yandex.com>
2016-07-11 08:32:32 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+var Yunitto = require('yunitto');
+
 function Desktop(config) {
+  if(typeof(config) != "object")
+   throw "nodeos-desktop: config is not a object";
+   
+  if(typeof(config.port) != "number")
+   throw "nodeos-desktop: config port is not a number";
+   
+  var yunitto = new Yunitto({httpport: config.port});
 }
 
 module.exports = Desktop;
